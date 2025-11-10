4eb3464b66a1e98a096eb8c5b624aea18a03bfc8
Alif Hakimi <alifhakimiazwan@gmail.com>
2025-11-10 23:02:24 +0800

    fix: remove baseURL from axios config to use relative URLs

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || "", // optional
+  // No baseURL - use relative URLs (works on any domain)
   headers: {
     "Content-Type": "application/json",
   },
